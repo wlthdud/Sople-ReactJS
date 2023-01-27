ca7ec609781737e671a0ffcefca392d7f1e9ea4c
wlthdud <thdudzi@naver.com>
2023-01-28 00:06:34 +0900

    feat: 댓글 컴포넌트 만들기 + 객체로 분리

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "지소영",
+        comment: "안녕하세요. 지소영입니다.",
+    },
+    {
+        name: "이혁진",
+        comment: "안녕하세요. 이혁진입니다.",
+    },
+    {
+        name: "헬로키티",
+        comment: "안녕하세요. 헬로키티입니다.",
+    },
+];
+
 function CommentList(props){
     return (
         <div>
-            <Comment name={"지소영"} comment={"안녕하세요. 지소영입니다."} />
-            <Comment name={"이혁진"} comment={"안녕하세요. 이혁진입니다."} />
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }
