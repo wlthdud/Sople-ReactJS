9a2394f5feaab3684a9e4416df81a013841558e3
wlthdud <thdudzi@naver.com>
2023-01-28 00:00:56 +0900

    feat: 댓글 컴포넌트 만들기 + 동적 변경

--- my-app/src/chapter_05/Comment.jsx
+++ my-app/src/chapter_05/Comment.jsx
@@ -43,10 +43,8 @@ function Comment(props){
             </div>
         
             <div style={styles.contentContainer}>
-                <span style={styles.nameText}>지소영</span>
-                <span style={styles.commentText}>
-                    제가 만든 첫 컴포넌트입니다.
-                </span>
+                <span style={styles.nameText}>{props.name}</span>
+                <span style={styles.commentText}>{props.comment}</span>
             </div>
         </div>
     );
--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -4,7 +4,8 @@ import Comment from "./Comment";
 function CommentList(props){
     return (
         <div>
-            <Comment />
+            <Comment name={"지소영"} comment={"안녕하세요. 지소영입니다."} />
+            <Comment name={"이혁진"} comment={"안녕하세요. 이혁진입니다."} />
         </div>
     );
 }
