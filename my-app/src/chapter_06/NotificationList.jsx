eae12f8a1ab721e33364b24dfc97f566c2ee804c
wlthdud <thdudzi@naver.com>
2023-01-31 00:10:23 +0900

    feat: 생명주기 함수 사용해보기

--- my-app/src/chapter_06/NotificationList.jsx
+++ my-app/src/chapter_06/NotificationList.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reservedNotifications = [
     {
+        id: 1,
         message: "안녕하세요, 오늘 일정을 알려드립니다.",
     },
     {
+        id: 2,
         message: "점심 식사 시간입니다.",
     },
     {
+        id: 3,
         message: "이제 곧 미팅이 시작됩니다.",
     },
 ];
@@ -19,21 +22,24 @@ class NotificationList extends React.Component{
     constructor(props){
         super(props);
 
-        this.state = {
-            notification: [],
+        this.state = {  //생성자에서는 앞으로 사용할 데이터를 state에 넣어서 초기화
+            notifications: [],
         };
     }
 
-    componentDidMount() {
-        const { notification } = this.state;
+    componentDidMount() {  //클래스 컴포넌트의 생명주기 함수 중 하나
+        const { notifications } = this.state;
         timer = setInterval(() => {
-            if(NotificationList.length < reservedNotifications.length){
+            if(notifications.length < reservedNotifications.length){
                 const index = notifications.length;
                 notifications.push(reservedNotifications[index]);
-                this.setState({
+                this.setState({  //state를 업데이트 하기 위한 setState() 함수 사용
                     notifications: notifications,
                 });
             }else{
+                this.setState({
+                    notifications: [],
+                });
                 clearInterval(timer);
             }
         }, 1000);
@@ -43,7 +49,13 @@ class NotificationList extends React.Component{
         return (
             <div>
                 {this.state.notifications.map((notification) => {
-                    return <Notification message={notification.message} />;
+                    return (
+                        <Notification 
+                            key={notification.id}
+                            id={notification.id}
+                            message={notification.message} 
+                        />
+                    );     
                 })}
             </div>
         );
