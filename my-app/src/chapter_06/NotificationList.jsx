c3fa1910f4a0e9f96e52153d745036f221947bbd
wlthdud <thdudzi@naver.com>
2023-01-30 22:36:53 +0900

    feat: NotificationList.jsx 추가

---
+++ my-app/src/chapter_06/NotificationList.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Notification from "./Notification";
+
+const reservedNotifications = [
+    {
+        message: "안녕하세요, 오늘 일정을 알려드립니다.",
+    },
+    {
+        message: "점심 식사 시간입니다.",
+    },
+    {
+        message: "이제 곧 미팅이 시작됩니다.",
+    },
+];
+
+var timer;
+
+class NotificationList extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            notification: [],
+        };
+    }
+
+    componentDidMount() {
+        const { notification } = this.state;
+        timer = setInterval(() => {
+            if(NotificationList.length < reservedNotifications.length){
+                const index = notifications.length;
+                notifications.push(reservedNotifications[index]);
+                this.setState({
+                    notifications: notifications,
+                });
+            }else{
+                clearInterval(timer);
+            }
+        }, 1000);
+    }
+
+    render(){
+        return (
+            <div>
+                {this.state.notifications.map((notification) => {
+                    return <Notification message={notification.message} />;
+                })}
+            </div>
+        );
+    }
+}
+
+export default NotificationList;
