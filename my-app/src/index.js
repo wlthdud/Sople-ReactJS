06a98cc3b0624be319be1e79d353ddf79ab0f089
wlthdud <thdudzi@naver.com>
2023-04-05 23:47:57 +0900

    feat: 클릭이벤트 처리하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -9,10 +9,11 @@ import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>,
   document.getElementById('root')
 );
