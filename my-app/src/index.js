f25885c16a59baa56bf9445f291da493ec01364a
wlthdud <thdudzi@naver.com>
2023-04-05 22:12:47 +0900

    feat: 훅을 사용한 컴포넌트 개발 추가

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,10 +8,11 @@ import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>,
   document.getElementById('root')
 );
