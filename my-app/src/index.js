831866f79daff4ce325261b3eb9f591df76d857e
wlthdud <thdudzi@naver.com>
2023-04-08 23:10:24 +0900

    feat: 섭씨온도와 화씨온도 표시하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,10 +13,11 @@ import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
